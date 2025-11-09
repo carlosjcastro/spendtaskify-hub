9a1748a59249a55f57cb9e0e678eb50f7717364d
Castro_Galante_Carlos_José <carlosjosecastro.p@gmail.com>
2025-11-09 20:41:10 -0300

    se crea y actualiza base de datos del proyecto

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://oyxzajcawnadddsfohez.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95eHphamNhd25hZGRkc2ZvaGV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyMzk3MTMsImV4cCI6MjA2MTgxNTcxM30.keNEyYQ4q-fF4V9BgsBkGGLtJEGMbmGe2ZPSUTUubAk';
+const supabaseUrl = 'https://ngkrrixfewuplvkqmzfw.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5na3JyaXhmZXd1cGx2a3FtemZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3MTk5ODcsImV4cCI6MjA3ODI5NTk4N30.qzrQFPt6GlgPxecS7sM-CM52r8qDvKiH8eflEHJoIv0';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
